bff980b192e77c699b42477c3a4d1a6fdd3ff1e9
Jeong Kim <joshkim83@outlook.com>
2016-08-04 12:44:13 -0400

    adding commit

---
+++ newAeolus/WeatherVC.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherVC.swift
+//  newAeolus
+//
+//  Created by Jeong Kim on 8/1/16.
+//  Copyright © 2016 Josh Kim. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
